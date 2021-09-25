35e3ec9080ccbf8c5abc74301275e6181e034e4b
mohammed2808219 <hkon20111@gmail.com>
2021-09-25 13:41:59 +0700

    ubah kedua

---
+++ LKP-5/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.myrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvCategory;
+    private ArrayList<President> list = new ArrayList<>();
+    private String title = "Mode List";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvCategory = findViewById(R.id.rv_category);
+        rvCategory.setHasFixedSize(true);
+
+        list.addAll(PresidentData.getListData());
+        setActionBarTitle("Mode List");
+        showRecyclerList();
+    }
+    private void showRecyclerList(){
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
+        listPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(listPresidentAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+    return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_list:
+                setActionBarTitle("Mode List");
+                showRecyclerList();
+
+                break;
+
+            case R.id.action_grid:
+                setActionBarTitle("Mode Grid");
+                showRecyclerGrid();
+                break;
+
+            case R.id.action_cardview:
+                setActionBarTitle("Mode CardView");
+                showRecyclerCardView();
+                break;
+        }
+    return super.onOptionsItemSelected(item);
+    }
+
+    private void showRecyclerGrid(){
+        rvCategory.setLayoutManager(new GridLayoutManager(this, 2));
+        GridPresidentAdapter gridPresidentAdapter = new GridPresidentAdapter(this);
+        gridPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(gridPresidentAdapter);
+    }
+
+    private void showRecyclerCardView(){
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        CardViewPresidentAdapter cardViewPresidentAdapter = new CardViewPresidentAdapter(this);
+        cardViewPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(cardViewPresidentAdapter);
+    }
+
+    private void setActionBarTitle(String title){
+        getSupportActionBar().setTitle(title);
+    }
+}
